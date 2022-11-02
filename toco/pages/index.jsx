fa31f99fc006d4f4bac7c1d4d3543f3dc8baeb8c
mangming2 <leejiho35@gmail.com>
2022-11-03 08:56:01 +0900

    update

--- toco/pages/index.jsx
+++ toco/pages/index.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useRef, useCallback } from "react";
+import styled from "styled-components";
+import useScrollFadeIn from "../src/hooks/useScrollFadeIn";
+const Home = () => {
+  const animatedItem = {
+    0: useScrollFadeIn("up", 1, 0),
+    1: useScrollFadeIn("up", 1, 0.2),
+    2: useScrollFadeIn("up", 1, 0.3),
+  };
 
-export default function Home() {
-  return <div>메인페이지</div>;
-}
+  return (
+    <>
+      <StyledFirst {...animatedItem[0]}>메인페이지</StyledFirst>
+      <StyledSecond {...animatedItem[1]}>메인페이지</StyledSecond>
+      <StyledThird {...animatedItem[2]}>메인페이지</StyledThird>
+    </>
+  );
+};
+
+const StyledFirst = styled.div`
+  color: black;
+  margin-top: 500px;
+  opacity: 0;
+  transition: all 0.5s;
+`;
+const StyledSecond = styled.div`
+  color: black;
+  margin-top: 500px;
+  opacity: 0;
+  transition: all 0.5s;
+`;
+const StyledThird = styled.div`
+  color: black;
+  margin-top: 500px;
+  opacity: 0;
+  transition: all 0.5s;
+`;
+
+export default Home;
--- toco/src/components/AppBar.jsx
+++ toco/src/components/AppBar.jsx
@@ -1,29 +1,34 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import styled from "styled-components";
 function AppBar() {
   const router = useRouter();
   return (
-    <>
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/ProjectFund">
-          <a>projectfund</a>
-        </Link>
-        <Link href="/PennyFund">
-          <a>pennyfund</a>
-        </Link>
-        <Link href="/About">
-          <a>About</a>
-        </Link>
-        <Link href="/MyPage">
-          <a>mypage</a>
-        </Link>
-      </div>
-    </>
+    <Wrap>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/ProjectFund">
+        <a>projectfund</a>
+      </Link>
+      <Link href="/PennyFund">
+        <a>pennyfund</a>
+      </Link>
+      <Link href="/About">
+        <a>About</a>
+      </Link>
+      <Link href="/MyPage">
+        <a>mypage</a>
+      </Link>
+    </Wrap>
   );
 }
 
 export default AppBar;
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  line-height: 80px;
+`;
